3e1b82ee9495d69a38b45a7b5cb3c87ed3a71201
binthang_cest <ybpior92@cest.re.kr>
2020-03-20 09:09:22 +0900

    자잘한 UI 수정

--- app/src/main/java/com/ybbae/diaryofservant/ui/timeline/TimelineScreen.kt
+++ app/src/main/java/com/ybbae/diaryofservant/ui/timeline/TimelineScreen.kt
@@ -4,14 +4,15 @@ import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.layout.Column
+import androidx.ui.layout.LayoutGravity
 import androidx.ui.layout.LayoutPadding
+import androidx.ui.material.FloatingActionButton
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.TopAppBar
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.ybbae.diaryofservant.R
-import com.ybbae.diaryofservant.ui.VectorImageButton
-import com.ybbae.diaryofservant.ui.lightThemeColors
+import com.ybbae.diaryofservant.ui.*
 
 @Composable
 fun TimelineScreen(openDrawer : () -> Unit) {
@@ -26,9 +27,18 @@ fun TimelineScreen(openDrawer : () -> Unit) {
 				})
 
 			VerticalScroller() {
-				Column(modifier = LayoutPadding(16.dp)) {
+				Column(modifier = LayoutPadding(16.dp) + LayoutFlexible(1f)) {
 					Text(text = "Timeline")
 				}
+			}
+			
+			FloatingActionButton(
+				onClick = {
+					navigateTo(DSScreen.DSWriteCardScreen)
+				},
+				modifier = LayoutGravity.End + LayoutPadding(end = 16.dp)
+			) {
+				VectorImage(id = R.drawable.ic_home, tint = (MaterialTheme.colors()).background)
 			}
 		}
 	}
